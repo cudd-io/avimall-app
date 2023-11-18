7da6915ecca19c4dfab42191e3571d6d5801c822
Erika <eatmyrahmen@gmail.com>
2023-11-18 15:52:59 -0500

    Remove deleted reference to settings/sidebar

--- src/lib/layout/index.ts
+++ src/lib/layout/index.ts
@@ -3,4 +3,3 @@ export { default as Header } from './Header.svelte';
 export { default as Footer } from './Footer.svelte';
 export { default as Shell } from './Shell.svelte';
 export { default as UserMenu } from './UserMenu.svelte';
-export { default as SettingsSidebar } from './settings/Sidebar.svelte';
