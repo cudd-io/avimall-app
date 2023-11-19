cb99973d69606e9c7313daa482da51a281bc4063
Erika <eatmyrahmen@gmail.com>
2023-11-19 07:34:26 -0500

    Remove references to deleted files

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,13 +1,19 @@
 // ui components
+
+// Shadcn components are named in lowercase by default and nothing good ever comes from changing the casing of a file in git.
+// For consistency, all UI new components should be named in lowercase. Existing UI components will eventually be renamed as well, and other components will stay capitalized.
 export { default as Container } from './ui/Container.svelte';
 export { default as Portal } from './ui/Portal.svelte';
 export { default as Alert } from './ui/Alert.svelte';
 export { default as HR } from './ui/HR.svelte';
+export * as Button from './ui/button';
+export * as Form from './ui/form';
+export * as Label from './ui/label';
+export * as RadioGroup from './ui/radio-group';
+export * as Select from './ui/select';
+export * as Switch from './ui/switch';
+export * as Textarea from './ui/textarea';
 
 // Sidebar components
 export { default as GlobalSidebar } from './sidebars/GlobalSidebar.svelte';
 export { default as RailSidebar } from './sidebars/RailSidebar.svelte';
-
-// form components
-export { default as FormElement } from './form/FormElement.svelte';
-export { default as Form } from './form/Form.svelte';
